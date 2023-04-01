3a9f40ed51cac409a0a33fa8aa2820a7f0f1e0cd
Zhiyuan Cao <czy0538@gmail.com>
2023-04-01 10:45:03 +0800

    2023-04-01

---
+++ codes/Tree/617.merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class Solution {
+public:
+    TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2) {
+        if(!root1){
+            return root2;
+        }
+        if(!root2){
+            return root1;
+        }
+        auto node = new TreeNode();
+        if (root1 && root2) {
+            node->val = root1->val + root2->val;
+            node->left = mergeTrees(root1->left, root2->left);
+            node->right = mergeTrees(root1->right, root2->right);
+        }
+        return node;
+    }
+};
