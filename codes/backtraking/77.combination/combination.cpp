4c910f07dd3a61aee68d17307af9790e2f7c32db
Zhiyuan Cao <czy0538@gmail.com>
2023-04-12 10:46:21 +0800

    2023-04-12

--- codes/backtraking/77.combination/combination.cpp
+++ codes/backtraking/77.combination/combination.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+// 没有剪枝的版本
+// class Solution {
+// public:
+//     vector<vector<int>> result;
+//     vector<int> path;
+//     vector<vector<int>> combine(int n, int k) {
+//         backtracing(1, n, k);
+//         return result;
+//     }
+//     void backtracing(int beg, int n, int k) {
+//         if (k == 0) {
+//             result.push_back(path);
+//             return;
+//         }
+//         for (int i = beg; i <= n; i++) {
+//             path.push_back(i);
+//             backtracing(i + 1, n, k - 1);
+//             path.pop_back();
+//         }
+//     }
+// };
+
+// 剪枝版本
 class Solution {
 public:
     vector<vector<int>> result;
@@ -14,7 +38,7 @@ public:
             result.push_back(path);
             return;
         }
-        for (int i = beg; i <= n; i++) {
+        for (int i = beg; i <= n && (n - i + 1) >= k; i++) {
             path.push_back(i);
             backtracing(i + 1, n, k - 1);
             path.pop_back();
